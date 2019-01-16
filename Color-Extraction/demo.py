19b6264bd2c131b1d56d1546b0692cd193bc282f
Rune Harlyk <36825685+runeharlyk@users.noreply.github.com>
2019-01-16 22:51:24 +0100

    Update demo.py

--- Color-Extraction/demo.py
+++ Color-Extraction/demo.py
@@ -1,1 +1,9 @@
+import CE
+from time import sleep
 
+E = CE.Extraction()
+
+RedExtraction = E.Extract("test.jpg", "demo.jpg",  0, 100)
+if RedExtraction:
+	print("Color extration went successfully! You can find you picture here:")
+	print(RedExtraction)
