c2258ef2e953738f3623935776f034830b39aabb
Rune Harlyk <36825685+runeharlyk@users.noreply.github.com>
2019-01-16 22:44:51 +0100

    Update demo.py

--- Simple-Motion-Detection/demo.py
+++ Simple-Motion-Detection/demo.py
@@ -1,1 +1,38 @@
+import MD
+import picamera
+from time import sleep
+import datetime as dt
 
+camera = picamera.PiCamera(resolution=(300, 400))
+M = MD.Motion()
+
+camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+camera.start_preview(resolution=(300, 400))
+SurOnGoing = True
+recording = False
+filePlacement = ""
+
+#Warm up time
+sleep(2)
+print("Starting surveillance")
+
+while SurOnGoing:
+	camera.capture(filePlacement + "baseStatus.jpg", resize=(320, 240))
+	sleep(3)
+	camera.capture(filePlacement + "newstatus.jpg", resize=(320, 240))
+	#camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+	thereIsMotion = M.Check(filePlacement + "baseStatus.jpg", filePlacement + "newstatus.jpg", 10, False)
+
+	if thereIsMotion:
+		if not recording:
+			print("Motion started")
+			#camera.stop_recording(filePlacement + "video.h264")
+			recording = True
+	else:
+		if recording:
+			print("Motion stopped")
+			#camera.stop_recording()
+			#await client.send_file(filePlacement + "video.h264")
+			recording = False
+	sleep(1)
